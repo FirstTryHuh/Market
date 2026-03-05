2e3ac57d358a908941c8d2ac8de4e458196fe530
apple <apple@MacBook-Pro-cua-apple.local>
2026-03-05 13:42:36 +0700

    Fixed

--- app/components/SmallComponent/side.tsx
+++ app/components/SmallComponent/side.tsx
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function Side(): React.JSX.Element {
     return(
-        <div>
-            <video autoPlay muted loop src="/BackGroundVid.mp4"></video>
-        </div>
+        <>
+            <video autoPlay muted loop src="/BackGroundVid.mp4" width="400"></video>
+        </>
     )
 }
